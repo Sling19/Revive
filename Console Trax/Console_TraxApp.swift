6ecd7de2f2a8df56baca41c2aebfbaef40bc3b11
Kyle <78189574+Sling19@users.noreply.github.com>
2025-08-16 10:37:37 -0400

    Initial Commit

---
+++ Console Trax/Console_TraxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Console_TraxApp.swift
+//  Console Trax
+//
+//  Created by Kyle on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Console_TraxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
